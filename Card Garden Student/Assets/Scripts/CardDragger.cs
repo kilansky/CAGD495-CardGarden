2add829f425bc9aa7b6146e4083ca8e719a5ca23
Kilan Larsen <kilanlarsen@gmail.com>
2020-09-06 15:50:14 -0700

    Project Upload

---
+++ Card Garden Student/Assets/Scripts/CardDragger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 startPos;
+    private Quaternion startRot;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Store the starting Position and Rotation of the card
+        startPos = transform.localPosition;
+        startRot = transform.localRotation;
+
+        //Set rotation to 0 so card is upright
+        transform.rotation = Quaternion.identity;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Follow mouse position while dragging
+        transform.position = eventData.position;//Input.mousePosition;     
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Invalid Placement - Move card back to starting position
+        transform.localPosition = startPos;
+        transform.localRotation = startRot;
+    }
+}
