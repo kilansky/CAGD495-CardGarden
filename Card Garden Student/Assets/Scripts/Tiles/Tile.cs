867d636d04400db02c4d32f892ab7cdbcb190215
jacobpicard <jacobpicard49@yahoo.com>
2020-09-08 14:03:05 -0700

    Tile declaration and changeOccupant logic

--- Card Garden Student/Assets/Scripts/Tiles/Tile.cs
+++ Card Garden Student/Assets/Scripts/Tiles/Tile.cs
@@ -1,46 +1,98 @@
-﻿using System.Collections;
+﻿/**
+* @author Jacob Picard
+* @date 9/8/2020
+**/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public enum TileType { empty, path, rock, trees, water };
-
+/**
+* @brief This class controls a tiles behavior in the grid.
+**/
 public class Tile : MonoBehaviour
-{   
-    public TileType tileType;
+{
+    public enum tileEnum
+	{
+		Null,
+		Lane,
+		Building
+	}	
+	public enum directionEnum
+	{
+		Zup,
+		Zdown,
+		Xnegative,
+		Xpositive,
+		None
+	}
+	public enum textures
+	{
+		grass,
+		rock,
+		tree,
+		water
+	}
+	
+    public tileEnum tileType;
+	public directionEnum direction;
+	public textures tileTexture;
+	
+	public GameObject occupant;
 
-    public Card storedCard;
-    public Transform spawnPos;
+    private Card storedCard;
+	public Tile nextTile;
+	
+	/**
+	* @brief instantiates a building on top of the tile based on the card played
+	**/
+	public void instantiateBuilding()
+	{
+		
+	}
+	/**
+	* @brief captures tile events
+	**/
+	public void eventController()
+	{
+		
+	}
+	
+	/**
+	* @brief Changes the texture on a tile.
+	**/
+	private void tileTypeSelector()
+	{
+		/*if (this.tileTexture == textures.rock)
+		{
+			this.gameObject.GetComponent<Renderer>().material = 
+		}*/
+	}
+	
+	/**
+	* @brief changes the tile this tile points to.
+	*/
+	public void directionChanger()
+	{
 
-    public Material empty;
-    public Material path;
-    public Material rock;
-    public Material trees;
-    public Material water;  
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(tileType == TileType.empty)
-        {
-            gameObject.GetComponent<Renderer>().material = empty;
-        }
-        else if (tileType == TileType.rock)
-        {
-            gameObject.GetComponent<Renderer>().material = rock;
-        }
-        else if (tileType == TileType.trees)
-        {
-            gameObject.GetComponent<Renderer>().material = trees;
-        }
-        else if (tileType == TileType.water)
-        {
-            gameObject.GetComponent<Renderer>().material = water;
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+	
+	/**
+	* @brief removes or adds an enemy or player character to the tile.
+	**/
+	public void changeOccupant()
+	{
+		if(this.occupant == default(GameObject))
+		{
+			RaycastHit hit;
+			Physics.BoxCast(this.transform.position,
+			transform.localScale, transform.up, out hit,
+			transform.rotation);
+			occupant = hit.collider.gameObject;
+		}
+		else
+		{
+			occupant = default(GameObject);
+		}
+	}
+	
 }
